af4fb6af4175da8dd8e8bdc202182d08c352dbb7
Flavio Gomes <flaviogomes@gmail.com>
2023-03-26 16:50:17 -0300

    v2
    Prompt: `Please add a button to mark a "todo" as done and move it to a new list. And also on both lists add a button to delete each "todo"`

--- app.js
+++ app.js
@@ -1,17 +1,51 @@
 const form = document.querySelector('#todo-form');
 const input = document.querySelector('#todo-input');
-const list = document.querySelector('#todo-list');
+const todoList = document.querySelector('#todo-list');
+const doneList = document.querySelector('#done-list');
 
 // Load existing todos from local storage
-const todos = JSON.parse(localStorage.getItem('todos')) || [];
+let todos = JSON.parse(localStorage.getItem('todos')) || [];
+let doneTodos = JSON.parse(localStorage.getItem('doneTodos')) || [];
 
 // Render todos in the list
 function renderTodos() {
-  list.innerHTML = '';
+  todoList.innerHTML = '';
   for (const todo of todos) {
     const li = document.createElement('li');
-    li.textContent = todo;
-    list.appendChild(li);
+    const span = document.createElement('span');
+    span.textContent = todo;
+    li.appendChild(span);
+    const doneButton = document.createElement('button');
+    doneButton.textContent = 'Done';
+    doneButton.addEventListener('click', () => {
+      markAsDone(todo);
+    });
+    li.appendChild(doneButton);
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deleteTodo(todo, todos);
+    });
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
+  }
+}
+
+// Render done todos in the list
+function renderDoneTodos() {
+  doneList.innerHTML = '';
+  for (const todo of doneTodos) {
+    const li = document.createElement('li');
+    const span = document.createElement('span');
+    span.textContent = todo;
+    li.appendChild(span);
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', () => {
+      deleteTodo(todo, doneTodos);
+    });
+    li.appendChild(deleteButton);
+    doneList.appendChild(li);
   }
 }
 
@@ -26,11 +60,40 @@ function addTodo() {
   }
 }
 
+// Mark a todo as done and move it to the done list
+function markAsDone(todo) {
+  const index = todos.indexOf(todo);
+  if (index > -1) {
+    doneTodos.push(todo);
+    localStorage.setItem('doneTodos', JSON.stringify(doneTodos));
+    todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    renderTodos();
+    renderDoneTodos();
+  }
+}
+
+// Delete a todo from the list and update local storage
+function deleteTodo(todo, list) {
+  const index = list.indexOf(todo);
+  if (index > -1) {
+    list.splice(index, 1);
+    if (list === todos) {
+      localStorage.setItem('todos', JSON.stringify(todos));
+      renderTodos();
+    } else {
+      localStorage.setItem('doneTodos', JSON.stringify(doneTodos));
+      renderDoneTodos();
+    }
+  }
+}
+
 // Handle form submission
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   addTodo();
 });
 
 // Render initial todos
 renderTodos();
+renderDoneTodos();
